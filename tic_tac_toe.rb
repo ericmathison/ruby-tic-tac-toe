564595b3b3998a49db87feb9ccf0112a1361574d
Eric Mathison <dev@ericmathison.io>
2020-04-16 18:19:01 -0700

    Prefer #raw over #to_s for internal board state

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,14 +1,15 @@
-class Board
+# position map:
+# 1 |  2 |  3
+# -----------
+# 4 |  5 |  6
+# -----------
+# 7 |  8 |  9
 
-    # position map:
-    # 1 |  2 |  3
-    # -----------
-    # 4 |  5 |  6
-    # -----------
-    # 7 |  8 |  9
+class Board
+  attr_reader :raw
 
   def initialize(str)
-    @board_string = strip_extraneous(str)
+    @raw = str.gsub(/[^xo-]/,'')
   end
 
   def self.[](str)
@@ -16,20 +17,10 @@ class Board
   end
 
   def move(piece, position)
-    @board_string[position - 1] = piece.to_s
+    @raw[position - 1] = piece.to_s
   end
 
   def position_open?(n)
-    strip_extraneous(@board_string)[n - 1] == '-'
-  end
-
-  def to_s
-    @board_string.chars.each_slice(3).map(&:join).join("\n")
-  end
-
-  private
-
-  def strip_extraneous(str)
-    str.gsub(/[^xo-]/,'')
+    @raw[n - 1] == '-'
   end
 end
--- tic_tac_toe_test.rb
+++ tic_tac_toe_test.rb
@@ -1,48 +1,38 @@
 require_relative 'tic_tac_toe.rb'
 require 'minitest/autorun'
 
+# position map:
+# 1 |  2 |  3
+# -----------
+# 4 |  5 |  6
+# -----------
+# 7 |  8 |  9
+
 class TestBoard < Minitest::Test
-
-    # position map:
-    # 1 |  2 |  3
-    # -----------
-    # 4 |  5 |  6
-    # -----------
-    # 7 |  8 |  9
-
   def setup
-    @board = Board['---
-                    ---
-                    ---']
+    @blank_board = Board['--- --- ---']
+    @board_ignores_strange_characters = Board["-abc123--\n -\n\n-- \n -xo"]
+    @board_state_one = Board['--- -o- x-x']
+    @board_state_two = Board['o-- --- --x']
   end
 
-  def test_to_s
-    board_output = "---\n---\n---"
-    assert_equal @board.to_s, board_output
-
-    board = Board['o--
-                   ---
-                   --x']
-    board_output = "o--\n---\n--x"
-    assert_equal board.to_s, board_output
+  def test_raw
+    assert_equal @blank_board.raw, '---------'
+    assert_equal @board_ignores_strange_characters.raw, '-------xo'
+    assert_equal @board_state_one.raw, '----o-x-x'
+    assert_equal @board_state_two.raw, 'o-------x'
   end
 
   def test_move
-    @board.move(:x, 9)
-    expected_board = Board['---
-                            ---
-                            --x']
-    assert_equal @board.to_s, expected_board.to_s
+    @blank_board.move(:x, 9)
+    assert_equal @blank_board.raw, '--------x'
+    @board_state_two.move(:o, 4)
+    assert_equal @board_state_two.raw, 'o--o----x'
   end
 
   def test_position_open?
-    1.upto(9) { |n| assert @board.position_open?(n) }
-
-    board = Board['---
-                   -o-
-                   x-x']
-
-    [1, 2, 3, 4, 6, 8].each { |n| assert board.position_open?(n) }
-    [5, 7, 9].each { |n| refute board.position_open?(n) }
+    1.upto(9) { |n| assert @blank_board.position_open?(n) }
+    [1, 2, 3, 4, 6, 8].each { |n| assert @board_state_one.position_open?(n) }
+    [5, 7, 9].each { |n| refute @board_state_one.position_open?(n) }
   end
 end
