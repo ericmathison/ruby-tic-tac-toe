84259724b51b042705e209a82e2ddd6dc2766c0f
Eric Mathison <dev@ericmathison.io>
2020-05-19 23:45:09 -0700

    Add gap between board and "Enter a move"

--- game.rb
+++ game.rb
@@ -6,6 +6,7 @@ input = nil
 
 until board.game_over? || input == 'q'
   puts UI.output(board)
+  puts
   print "Enter a move (from 1-9) or quit (q): "
   input = STDIN.gets.strip
   board.move(board.current_player, input.to_i)
