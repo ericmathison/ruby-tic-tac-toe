814fd1dfa986e7b5a897b46509f5b2175b101fd6
Eric Mathison <dev@ericmathison.io>
2020-05-29 23:58:01 -0700

    Use single quotes

--- game.rb
+++ game.rb
@@ -8,7 +8,7 @@ until board.game_over? || input == 'q'
   system 'clear'
   puts UI.output(board)
   puts
-  print "Enter a move (from 1-9) or quit (q): "
+  print 'Enter a move (from 1-9) or quit (q): '
   input = STDIN.gets.strip
   board.move(board.current_player, input.to_i) unless input == 'q'
   puts
